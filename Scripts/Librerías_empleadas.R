64de5762bdf2cb47eaabd9eb18de83660ff692f4
juditponce <jponcecasanas@gmail.com>
2025-01-20 14:19:28 +0100

    Update Librerías_empleadas.R

--- Scripts/Librerías_empleadas.R
+++ Scripts/Librerías_empleadas.R
@@ -2,4 +2,6 @@
 
 library(dplyr) # manipular tablas y datos
 library(ggplot2) # gráficos
-
+library(tibble) #Conversión de columnas en nombres de fila
+library(pheatmap) # Generación de heatmaps
+library(stats) # Funciones estadísticas base para estandarización de datos.
