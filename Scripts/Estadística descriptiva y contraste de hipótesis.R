ee3277013a4d39920cc37edbaf8c9873fa38329a
mizquierdoperez <mizquierdoperezuz@gmail.com>
2025-01-24 18:14:03 +0100

    Update and rename Estadística descriptiva y contraste de hipótesis to Estadística descriptiva y contraste de hipótesis.R

---
+++ Scripts/Estadística descriptiva y contraste de hipótesis.R
@@ -0,0 +1,145 @@
+Cargamos el dataset (data) con nuestros datos de interes:
+
+data <- read.csv("https://raw.githubusercontent.com/AlfonsoBravoPaneque/Actividad-2/refs/heads/main/Data/Dataset%20expresi%C3%B3n%20genes.csv")
+
+df_genes <- select(data, trat, tumor, starts_with("AQ_")) # con select() de dplyr se selecciona lo deseado
+
+table_2 <- df_genes %>%
+  tbl_strata(
+    strata = trat, # se estratifican los datos por tratamiento
+    .tbl_fun = ~ .x %>%
+      tbl_summary(
+        by = tumor, # se agrupan los datos por tumor
+        statistic = all_continuous() ~ "{median} ({p25} - {p75})", # se calcula la mediana y el RIC
+        type = all_continuous() ~ "continuous",
+        digits = all_continuous() ~ function(x) format(x, digits = 3, scientific = TRUE) # formato científico
+      ) %>%
+      add_p( # se añade una columna con el p valor
+        test = list(all_continuous() ~ "kruskal.test"), # se emplea la prueba de Krustal-Wallis
+        pvalue_fun = ~ style_pvalue(.x, digits = 3)  
+      )
+  )
+
+table_2_gt <- table_2 %>%
+  as_gt() %>% # se convierte la tabla a un objeto gt para poder personalizarla
+  tab_header( # se agrega el título a la tabla
+    title = md("**Tabla 2. Estadísticas descriptivas y contraste de hipótesis de los genes 
+               según el tipo de tumor y tratamiento.**")
+  ) %>%
+  cols_label( # se nombra la columna
+    label = md("**Genes**")
+  ) %>%
+  tab_options( # se configura el aspecto global de la tabla
+    table.border.top.style = "none",
+    table.border.bottom.style = "none",
+    table.font.style = "Arial",
+    column_labels.background.color = "gray",
+    heading.align = "left"
+  ) %>%
+  tab_style( # se aplican estilos personalizados
+    style = cell_borders(
+      sides = c("left", "right", "top", "bottom"),
+      color = "gainsboro",
+      weight = px(1.6)
+    ),
+    locations = list(
+      cells_body(
+        columns = everything()
+      ),
+      cells_column_labels(
+        columns = everything()
+      ),
+      cells_column_spanners(
+        spanners = everything()
+      )
+    )
+  ) %>%
+  tab_style( # se aplican estilos personalizados
+    cell_text(
+      align = "center",
+      v_align = "middle"
+    ),
+    locations = cells_column_labels(
+      columns = label
+    )
+  ) %>%
+  cols_align( # se alinean las columnas al centro
+    align = "center"
+  ) %>%
+  tab_footnote( # se introduce una nota al pie de la tabla
+    footnote = "Los valores p fueron calculados empleando la prueba de Krustal-Wallis."
+  )
+
+table_2_gt
+
+ediana_edad <- quantile(data$edad, probs = 0.5, na.rm = TRUE)
+
+data <- data %>%
+  mutate(edad_cat = if_else(edad < mediana_edad, 
+                           "Edad_cat < percentil 50", 
+                           "Edad_cat ≥ percentil 50"))
+
+data_genes <- data %>%
+  select(edad_cat, starts_with("AQ_"))
+
+table_3 <- data_genes %>%
+  tbl_summary(
+    by = edad_cat,  # variable de agrupación
+    statistic = list(all_continuous() ~ "{median} ({p25} - {p75})"), # se calcula la mediana y el RIC
+    digits = list(all_continuous() ~ function(x) format(x, digits = 3, scientific = TRUE)), # científico
+    missing = "no"
+  ) %>%
+  add_p(
+    test = list(all_continuous() ~ "wilcox.test"), # U de Mann-Whitney (Wilcoxon para muestras independientes)
+    pvalue_fun = ~ style_pvalue(.x, digits = 3)
+  )
+table_3_gt <- table_3 %>%
+  as_gt() %>% # se convierte la tabla a un objeto gt para poder personalizarla
+  tab_header( # se agrega el título a la tabla
+    title = md("**Tabla 3. Estadísticas descriptivas y contraste de hipótesis de los genes según la edad.**")
+  ) %>%
+  cols_label( # se nombra la columna
+    label = md("**Genes**")
+  ) %>%
+  tab_options( # se configura el aspecto global de la tabla
+    table.border.top.style = "none",
+    table.border.bottom.style = "none",
+    table.font.style = "Arial",
+    column_labels.background.color = "gray",
+    heading.align = "left"
+  ) %>%
+  tab_style( # se aplican estilos personalizados
+    style = cell_borders(
+      sides = c("left", "right", "top", "bottom"),
+      color = "gainsboro",
+      weight = px(1.6)
+    ),
+    locations = list(
+      cells_body(
+        columns = everything()
+      ),
+      cells_column_labels(
+        columns = everything()
+      ),
+      cells_column_spanners(
+        spanners = everything()
+      )
+    )
+  ) %>%
+  tab_style( # se aplican estilos personalizados
+    cell_text(
+      align = "center",
+      v_align = "middle"
+    ),
+    locations = cells_column_labels(
+      columns = c(label, p.value)
+    )
+  ) %>%
+  cols_align( # se alinean las columnas al centro
+    align = "center"
+  ) %>%
+  tab_footnote( # se introduce una nota al pie de la tabla
+    footnote = "Los valores p fueron calculados empleando la prueba de U de Mann-Whitney."
+  )
+
+table_3_gt
